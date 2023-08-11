e6a1a27feda6c7fa67d49fdbba195b47c4a6ed04
Ali Ghaleyan <mr.ali.serjik@gmail.com>
2023-08-11 12:18:58 +0330

    refactor: update menu mobile preview

--- src/components/Layout/BasePageLayout/components/Menu/Menu.tsx
+++ src/components/Layout/BasePageLayout/components/Menu/Menu.tsx
@@ -4,13 +4,16 @@ import { EmailIcon, TelephoneIcon } from "@/components/Icons";
 const Menu = () => {
   return (
     <div className="px-4 pt-4">
-      <div className="flex items-center justify-between rounded-full bg-mindful-gray-10 p-2">
-        <div className="flex gap-2 text-mindful-gray-80">
+      <div className="flex items-center justify-between rounded-full bg-mindful-gray-10 p-3.5 md:p-2">
+        <div className="hidden gap-2 text-mindful-gray-80 md:flex">
           <ButtonIcon icon={<TelephoneIcon width={24} height={24} />} />
           <ButtonIcon icon={<EmailIcon width={24} height={24} />} />
         </div>
-        <div className="w-[9.375rem]">
+        <div className="hidden text-empathy-orange-80 md:block">
           <Logo />
+        </div>
+        <div className="text-mindful-gray-60 md:hidden">
+          <Logo hideTitle />
         </div>
         <div className="flex items-center gap-2">
           <p className="text-lg font-semibold">Main Menu</p>
