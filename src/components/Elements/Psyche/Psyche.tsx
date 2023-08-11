bc502c7ef6db95bcc23d42fb59555bb59dfff887
Ali Ghaleyan <mr.ali.serjik@gmail.com>
2023-08-11 20:12:09 +0330

    fix: typescript error

--- src/components/Elements/Psyche/Psyche.tsx
+++ src/components/Elements/Psyche/Psyche.tsx
@@ -1,6 +1,7 @@
 import { PsycheIcon } from "@/components/Icons";
+import { SVGProps } from "react";
 
-const Psyche = (props: React.HtmlHTMLAttributes<SVGElement>) => {
+const Psyche = (props: SVGProps<SVGSVGElement>) => {
   return <PsycheIcon width={12} height={12} {...props} />;
 };
 
