94c496985fe86c1bdd5f7d5d3a2aee1e84d9cb8e
Ali Ghaleyan <mr.ali.serjik@gmail.com>
2023-08-11 12:08:08 +0330

    fix: remove useQueriesIsLoading hook

--- src/hooks/useQueriesIsLoading.ts
+++
@@ -1,9 +0,0 @@
-import { UseQueryResult } from "@tanstack/react-query/src/types";
-
-export const useQueriesIsLoading = (queries: UseQueryResult[]) => {
-  for (const query of queries) {
-    if (query.isLoading) return true;
-  }
-
-  return false;
-};
