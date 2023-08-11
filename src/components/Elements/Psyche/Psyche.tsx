6bc116c9c6918b17be4dd2cac9f63cf8a953ba36
Ali Ghaleyan <mr.ali.serjik@gmail.com>
2023-08-11 12:04:25 +0330

    feat: add Psyche component

---
+++ src/components/Elements/Psyche/Psyche.tsx
@@ -0,0 +1,7 @@
+import { PsycheIcon } from "@/components/Icons";
+
+const Psyche = (props: React.HtmlHTMLAttributes<SVGElement>) => {
+  return <PsycheIcon width={12} height={12} {...props} />;
+};
+
+export default Psyche;
