5cbccb3a14f0598b3ed3bd3635e5482ad9a6e72d
Ali Ghaleyan <mr.ali.serjik@gmail.com>
2023-08-21 12:05:43 +0330

    fix: CTA image container

--- src/components/Pages/Home/components/CTA/components/ImageContainer.tsx
+++ src/components/Pages/Home/components/CTA/components/ImageContainer.tsx
@@ -1,7 +1,13 @@
+import classNames from "classnames";
+
 const ImageContainer = () => {
   return (
     <div
-      className="ml-4 h-[27.688rem] rounded-bl-4xl rounded-tl-4xl border-8 border-r-0 border-mindful-gray-80 bg-cover shadow-2xl md:ml-0 md:h-[20rem] md:rounded-t-4xl md:rounded-bl-none md:border-r-8 lg:h-[30rem] lg:rounded-t-6xl xl:h-[42.75rem]"
+      className={classNames(
+        "ml-4 h-[27.688rem] bg-cover shadow-2xl md:ml-0 md:h-[20rem]  lg:h-[30rem] xl:h-[42.75rem]",
+        "rounded-bl-4xl rounded-tl-4xl md:rounded-t-4xl md:rounded-bl-none lg:rounded-t-6xl",
+        "border-8 border-r-0 border-mindful-gray-80 md:border-b-0 md:border-r-8"
+      )}
       style={{ backgroundImage: "url('/cta/dashboard.png')" }}
     />
   );
