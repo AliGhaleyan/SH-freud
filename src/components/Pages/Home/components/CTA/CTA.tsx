5b91b415fcee3c6bb23692a38a83ba91e9da0e8d
Ali Ghaleyan <mr.ali.serjik@gmail.com>
2023-08-21 12:47:59 +0330

    refactor: add overflow hidden to CTA container

--- src/components/Pages/Home/components/CTA/CTA.tsx
+++ src/components/Pages/Home/components/CTA/CTA.tsx
@@ -6,7 +6,7 @@ const CTA = () => {
   return (
     <Container
       parentClassName="pt-4 pb-0"
-      className="grid gap-y-12 rounded-6xl bg-empathy-orange-40 py-[5.125rem] md:gap-y-14 md:px-[5.25rem] md:pb-0 md:pt-32"
+      className="grid gap-y-12 overflow-hidden rounded-6xl bg-empathy-orange-40 py-[5.125rem] md:gap-y-14 md:px-[5.25rem] md:pb-0 md:pt-32"
     >
       <Heading />
       <ImageContainer />
