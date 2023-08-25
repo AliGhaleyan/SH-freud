4787dccc482be9b7ab01bab6ba039aec71c77dfd
Ali Ghaleyan <mr.ali.serjik@gmail.com>
2023-08-25 12:29:05 +0330

    feat: add title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>SH freud</title>
+      </Head>
       <body className="font-body">
         <Main />
         <NextScript />
