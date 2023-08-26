2e50aa6a0e3bab7b6a0921ed89fe56f32b0bd6f9
Ali Ghaleyan <mr.ali.serjik@gmail.com>
2023-08-26 13:17:14 +0330

    refactor: improve tags md

--- src/components/Pages/Home/components/Benefits/components/SecondBenefit/components/Tags.tsx
+++ src/components/Pages/Home/components/Benefits/components/SecondBenefit/components/Tags.tsx
@@ -8,7 +8,7 @@ const Tags = () => {
       className={classNames(
         "absolute z-[1050] flex gap-2 whitespace-nowrap text-mindful-gray-80",
         "top-[10.5rem] md:top-[13.125rem] xl:top-[16.75rem]",
-        "pl-14 md:pl-8 lg:pl-40 xl:pl-32"
+        "pl-14 md:pl-24 lg:pl-40 xl:pl-32"
       )}
     >
       <Tag className="my-auto bg-white" icon={<TelephoneIcon />} size="sm">
