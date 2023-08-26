d6b7ef135253797ade0169ae7198c7a8a1444c45
Ali Ghaleyan <mr.ali.serjik@gmail.com>
2023-08-26 13:39:35 +0330

    refactor: add theme to progressbar

--- src/components/Elements/ProgressBar/ProgressBar.tsx
+++ src/components/Elements/ProgressBar/ProgressBar.tsx
@@ -1,14 +1,28 @@
+import classNames from "classnames";
+
 type Props = {
   percent: number;
+  theme?: "gray" | "yellow";
+  className?: string;
+};
+
+const THEME_CLASS: Record<Props["theme"], string> = {
+  gray: "progress-bar-gray",
+  yellow: "progress-bar-yellow",
 };
 
 const ProgressBar = (props: Props) => {
+  const theme = props.theme || "gray";
+
   return (
-    <div className="h-2 w-full bg-mindful-gray-20">
-      <div
-        style={{ width: `${props.percent}%` }}
-        className="h-full bg-mindful-gray-80"
-      />
+    <div
+      className={classNames(
+        "progress-bar",
+        THEME_CLASS[theme],
+        props.className
+      )}
+    >
+      <div style={{ width: `${props.percent}%` }} className="h-full" />
     </div>
   );
 };
